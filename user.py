9c43a2c17144f3729df33be08cf6db70aac3fc0b
Lee Arnold <lee.arnoldiii@gmail.com>
2020-06-03 17:30:04 -0500

    update to stucture of classmethod for user.py

--- user.py
+++ user.py
@@ -2,13 +2,14 @@ from database import CursorFromConnectionFromPool
 
 class User:
     # Update user to include any additional information/ data for DB
-    def __init__(self, email, first_name, last_name, date_of_birth, gender, height):
+    def __init__(self, email, first_name, last_name, date_of_birth, gender, height, id):
         self.email = email
         self.first_name = first_name
         self.last_name = last_name
         self.date_of_birth = date_of_birth
         self.gender = gender
         self.height = height
+        self.id = id
 
     # Not sure this is needed yet, but have it just in case
     def __str__(self):
@@ -20,11 +21,12 @@ class User:
                           (self.email, self.first_name, self.last_name))
         
 
-    # @classmethod
-    # def load_from_db_by_email(cls, email):
-    #     with CursorFromConnectionFromPool() as cursor:
-    #         cursor.execute('SELECT * FROM users WHERE email=%s', (email,))
-    #         user_data = cursor.fetchone()
-    #         return cls(email=user_data[1], first_name=user_data[2],
-    #                     last_name=user_data[3], oauth_token=user_data[4],
-    #                     oauth_token_secret=user_data[5], id=user_data[0])
+    @classmethod
+    def load_from_db_by_email(cls, email):
+        with CursorFromConnectionFromPool() as cursor:
+            cursor.execute('SELECT * FROM users WHERE email=%s', (email,))
+            user_data = cursor.fetchone()
+            return cls(email=user_data[1], first_name=user_data[2],
+                        last_name=user_data[3], date_of_birth=user_data[4],
+                        gender=user_data[5], height=user_data[6],
+                        id=user_data[0])
