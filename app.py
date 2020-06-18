7343ca52fbd781dade8c557449f744170c292921
Lee Arnold <lee.arnoldiii@gmail.com>
2020-06-17 22:50:47 -0500

    making changes to revamp db connection. more changes coming.

--- app.py
+++ app.py
@@ -1,33 +1,33 @@
 from flask import Flask, render_template, session, redirect, request, url_for, g
-# from user import User
-# from database import Database
+from user import User
+from database import Database
 
-class User:
-    def __init__(self, id, username, password):
-        self.id = id
-        self.username = username
-        self.password = password
+# class User:
+#     def __init__(self, id, username, password):
+#         self.id = id
+#         self.username = username
+#         self.password = password
 
-    def __repr__(self):
-        return f'<User: {self.username}>'
+#     def __repr__(self):
+#         return f'<User: {self.username}>'
 
-users = []
-users.append(User(id=1, username='Tait', password='password'))
-users.append(User(id=2, username='Lee', password='secret'))
+# users = []
+# users.append(User(id=1, username='Tait', password='password'))
+# users.append(User(id=2, username='Lee', password='secret'))
 
 
 app = Flask(__name__)
 app.secret_key = 'youdontgettoknowthis'
 
-# Database.initialise(database='getting_fit_tracker', user='postgres', password='Outsmart1!@', host='localhost')
+Database.initialise(database='getting_fit_tracker', user='postgres', password='Outsmart1!@', host='localhost')
 
 @app.before_request
 def before_request():
     g.user = None
 
-    if 'user_id' in session:
-        user = [x for x in users if x.id == session['user_id']][0]
-        g.user = user
+    # if 'user_id' in session:
+    #     user = [x for x in users if x.id == session['user_id']][0]
+    #     g.user = user
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -37,21 +37,27 @@ def login():
         username = request.form['username']
         password = request.form['password']
 
-        user = [x for x in users if x.username == username][0]
-        if user and user.password == password:
-            session['user_id'] = user.id
-            return redirect(url_for('profile'))
+        # user = [x for x in users if x.username == username][0]
+        # if user and user.password == password:
+        #     session['user_id'] = user.id
+        #     return redirect(url_for('profile'))
 
         return redirect(url_for('login'))
 
-    return render_template('login2.html')
+    return render_template('login.html')
+
+@app.route('/registration')
+def registration():
+    return render_template('registration.html')
 
 @app.route('/profile')
 def profile():
     if not g.user:
         return redirect(url_for('login'))
         
-    return render_template('profile.html')
+    return render_template('dashboard.html')
+
+
 
 app.run(port=4995, debug=True)
 
--- user.py
+++ user.py
@@ -2,18 +2,20 @@ from database import CursorFromConnectionFromPool
 
 class User:
     # Update user to include any additional information/ data for DB
-    def __init__(self, email, first_name, last_name, date_of_birth, gender, height, id):
+    def __init__(self, email, first_name, last_name, date_of_birth, gender, height, username, password, id):
         self.email = email
         self.first_name = first_name
         self.last_name = last_name
         self.date_of_birth = date_of_birth
         self.gender = gender
         self.height = height
+        self.username = username
+        self.password = password
         self.id = id
 
     # Not sure this is needed yet, but have it just in case
     def __str__(self):
-        pass
+        return f'<User {self.username}>'
 
     def save_to_db(self):
         with CursorFromConnectionFromPool() as cursor:
